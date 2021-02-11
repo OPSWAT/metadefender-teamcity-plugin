0af70901f55fb66e3cdae248b7fdfd3d05b36627
Péter Horváth <phorvath@opswat.com>
2021-02-11 14:23:58 +0100

    Update version and add automatic artifact naming

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+VERSION=$(grep -oP '<version>\K.*(?=</version>)' teamcity-plugin.xml)
+
 docker build -t metadefender-teamcity-plugin .
 
 container=$(docker run -id metadefender-teamcity-plugin)
-docker cp $container:/mps/target/metadefender-plugin.zip metadefender-plugin.zip
+docker cp $container:/mps/target/metadefender-plugin.zip metadefender-tc-plugin-$VERSION.zip
 docker rm -v $container -f
 
 id=$(docker images -q metadefender-teamcity-plugin)
